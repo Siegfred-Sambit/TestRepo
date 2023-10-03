b270c84fff750e95660b656549b8f17d6418387f
Sam <bhuyan.sambit@gmail.com>
2023-10-03 16:08:22 +0530

    Create test.sh

---
+++ .github/scripts/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e -f
+
+#BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#FOLDER="$( cd "$BASE/../.." >/dev/null 2>&1 && pwd )"
+#echo $FOLDER
+#LIST_COMMANDS=$FOLDER/list-command.txt
+#COMMAND_LIST=$FOLDER/command_list.txt
+#export TG_ACTION="plan"
+
+#clean() {
+    #rm -f COMMAND_LIST
+#}
+
+#append_command() {
+    #local command="$1"
+    #echo "$command" >> $COMMAND_LIST
+#}
+
+execute_command() {
+    while IFS= read -r command; do
+        echo "Executing command: $command"
+        $command
+    done < like.txt
+}
+
+==============================================
+echo "Start plan process"
+
+echo "-> Generate all plans"
+execute_command
+==============================================
